40b3a0fa73f3f466a807f294dbe8d76f70380797
Walter <wxm7692@mavs.uta.edu>
2024-10-12 17:11:32 -0500

    B

--- P1.py
+++ P1.py
@@ -1,6 +1,5 @@
 #login and sign up validation
-
 import streamlit as st
-
-# Load an image from the assets folder
-st.image("assets/P1.png", caption="P1_page")
+def call_P1():
+    # Load an image from the assets folder
+    st.image("assets/P1.png", caption="P1_page")
