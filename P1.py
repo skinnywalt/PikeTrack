323851aec56f13a7897eb6b1637ea353cb519640
Sujal7002 <112437018+Sujal7002@users.noreply.github.com>
2024-10-12 23:15:15 +0000

    Innstructions added in P1

--- P1.py
+++ P1.py
@@ -3,3 +3,8 @@ import streamlit as st
 def call_P1():
     # Load an image from the assets folder
     st.image("assets/P1.png", caption="P1_page")
+
+    
+    
+    #Return True if Signup
+    #Return Flase if Login
