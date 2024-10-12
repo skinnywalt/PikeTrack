60940195cfcb62e71a35af08a5385e4b0839e06c
Sujal7002 <112437018+Sujal7002@users.noreply.github.com>
2024-10-12 22:28:40 +0000

    update

--- P1.py
+++ P1.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-#login and sign up validation
-import streamlit as st
-def call_P1():
-    # Load an image from the assets folder
-    st.image("assets/P1.png", caption="P1_page")
-=======
 import streamlit as st
 
 def call_P1():
     # Load an image from the assets folder
-    st.image("assets/P1.png", caption="P1_page")
->>>>>>> 1e8b563 (Mock updates for main and P1 and P2 is scratch)
+    st.image("P1.png", caption="P1_page")
